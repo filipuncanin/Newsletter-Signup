e0d6d57a95de0ddb758287a91ed656f7dd139f0f
Filip Uncanin <filipuncanin@gmail.com>
2023-02-18 23:08:05 +0100

    Initial Commit

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const request = require('request');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/signup.html');
+});
+
+app.post('/', function(req, res) {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: 'subscribed', // 'pending' - send confirmation email
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const dataPost = JSON.stringify(data);
+
+    const options = {
+        url: 'https://us21.api.mailchimp.com/3.0/lists/b523be9fee', // 'https://<server(end of API key)>.api.mailchimp.com/3.0/lists/<list-id>'
+        method: 'POST',
+        headers: {
+            Authorization: 'auth 0e9a81e9b975aa1dfa696baf79dae98d-us21'
+        },
+        body: dataPost
+    }
+
+    request(options, (err, response, body) => {
+        if(err) { 
+            res.sendFile(__dirname + '/failure.html');
+        }
+        else {
+            if(response.statusCode == 200){
+                res.sendFile(__dirname + '/success.html');
+                console.log(response);
+            } else {
+                res.sendFile(__dirname + '/failure.html');
+            }
+        }
+    })
+});  
+
+app.post('/failure', function(req, res) {
+    res.redirect('/');
+});
+
+app.listen(process.env.PORT || 3000, function (){
+    console.log('Server is running.');
+});
+
+// API Key
+// 0e9a81e9b975aa1dfa696baf79dae98d-us21
+
+// List ID
+// b523be9fee
